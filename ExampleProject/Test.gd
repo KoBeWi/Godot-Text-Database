f42c0f9cf56d43181c72c5ee4dc24805ca22236b
kobewi <kobewi4e@gmail.com>
2021-11-29 01:14:24 +0100

    Formatting etc.

---
+++ ExampleProject/Test.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+func _ready() -> void:
+	var icons := TextDatabase.new()
+	icons.mandatory_properties = ["color"]
+	icons.default_properties = {"scale": Vector2(1, 1), "scalef": 1.0}
+	icons.load_from_path("Icons.cfg")
+	icons.load_from_path("Icons.json")
+	
+	for entry in icons.get_array():
+		var icon := Sprite.new()
+		icon.name = entry.name
+		icon.texture = preload("res://icon.png")
+		icon.position = Vector2(64, 64 + get_child_count() * 72)
+		icon.modulate = Color(entry.color)
+		icon.scale = entry.scale * entry.scalef
+		add_child(icon)
