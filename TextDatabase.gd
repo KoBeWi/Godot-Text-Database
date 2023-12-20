4ae7a8e8335e715b8ec8671ab23b92f450322d0d
Adriaan de Jongh <adriaan@Adriaans-MacBook.local>
2023-12-20 16:32:15 +0100

    added static types where I think I knew what was going on, added @warning_ignore to the rest

--- TextDatabase.gd
+++ TextDatabase.gd
@@ -8,10 +8,10 @@ class_name TextDatabase
 ## Note that none of the data validation is performed in release builds.
 
 ## ID property for the entry. Each entry has an unique ID, starting from 0.
-var id_name := "id"
+var id_name: String = "id"
 
 ## Name property for an entry, i.e. you can find entry name under this property. Used as key when converting to Dictionary.
-var entry_name := "name"
+var entry_name: String= "name"
 
 ## If true, property types will be validated when possible (including checking against default properties). Automatically set to true if any mandatory or valid property has a type provided.
 var is_typed: bool
@@ -35,17 +35,18 @@ var reserve_validate: Callable
 var postprocess_entry: Callable
 
 ## Adds a mandatory property with optionally provided type.
-func add_mandatory_property(property: String, type: int = TYPE_MAX):
+func add_mandatory_property(property: String, type: int = TYPE_MAX) -> void:
 	assert(not __property_exists(property), "Property '%s' already exists." % property)
 	__mandatory_properties.append([property, type])
 
 ## Adds a valid property with optionally provided type.
-func add_valid_property(property: String, type: int = TYPE_MAX):
+func add_valid_property(property: String, type: int = TYPE_MAX) -> void:
 	assert(not __property_exists(property), "Property '%s' already exists." % property)
 	__valid_properties.append([property, type])
 
 ## Adds a valid property with a default value. Enforces type, unless [param typed] is false.
-func add_default_property(property: String, default, typed := true):
+@warning_ignore("untyped_declaration", "inferred_declaration")
+func add_default_property(property: String, default, typed := true) -> void:
 	add_valid_property(property, typeof(default) if typed else TYPE_MAX)
 	__default_values[property] = default
 
@@ -54,18 +55,22 @@ func _initialize() -> void:
 	pass
 
 ## Virtual. Called for every entry before it is processed (validated, checked for mandatory entries etc.).
+@warning_ignore("unused_parameter")
 func _preprocess_entry(entry: Dictionary) -> void:
 	pass
 
 ## Virtual. If is_validated is true, this will be called in addition to regular validation. Useful for providing special rules that aren't possible by default.
+@warning_ignore("unused_parameter")
 func _additional_validate(entry: Dictionary, property: String) -> String:
 	return ""
 
 ## Virtual. If is_validated is true, this will be called if regular validation fails. Can be used for custom entries that follow non-standard format.
+@warning_ignore("unused_parameter")
 func _reserve_validate(entry: Dictionary, property: String) -> bool:
 	return false
 
 ## Virtual. Called for every entry after it is processed.
+@warning_ignore("unused_parameter")
 func _postprocess_entry(entry: Dictionary) -> void:
 	pass
 
@@ -74,14 +79,15 @@ func get_array() -> Array[Dictionary]:
 	return __data
 
 ## Returns the database as a Dictionary, where entry_name is used for key. Fails if entry_name is empty. Use skip_unnamed to automatically skip unnamed entries.
-func get_dictionary(skip_unnamed := false) -> Dictionary:
+func get_dictionary(skip_unnamed: bool = false) -> Dictionary:
 	if entry_name.is_empty():
 		push_error("Can't create Dictionary if data is unnamed.")
 		return {}
 
 	if __data_dirty:
 		__dict.clear()
-		for entry in __data:
+		@warning_ignore("inferred_declaration")
+		for entry: Dictionary in __data:
 			if not entry_name in entry:
 				assert(not skip_unnamed, "Entry has no name, can't create Dictionary key.")
 				continue
@@ -98,6 +104,7 @@ func size() -> int:
 	return __data.size()
 
 ## Returns whether the property is valid.
+@warning_ignore("untyped_declaration")
 func is_property_valid(entry: Dictionary, property: String, value = null) -> bool:
 	if not OS.is_debug_build():
 		return true
@@ -107,15 +114,17 @@ func is_property_valid(entry: Dictionary, property: String, value = null) -> boo
 	
 	var valid: bool = property == entry_name or property == id_name
 	if not valid:
-		for prop in __mandatory_properties:
+		for prop: Array in __mandatory_properties:
 			if prop[0] == property:
+				@warning_ignore("unsafe_call_argument")
 				assert(not is_typed or __match_type(typeof(value), prop[1]), "Invalid type of property '%s' in entry '%s'." % [property, entry.get(entry_name)])
 				valid = true
 				break
 	
 	if not valid:
-		for prop in __valid_properties:
+		for prop: Array in __valid_properties:
 			if prop[0] == property:
+				@warning_ignore("unsafe_call_argument")
 				assert(not is_typed or __match_type(typeof(value), prop[1]), "Invalid type of property '%s' in entry '%s'." % [property, entry.get(entry_name)])
 				valid = true
 				break
@@ -138,27 +147,28 @@ func is_property_valid(entry: Dictionary, property: String, value = null) -> boo
 
 ## Creates a TextDatabase from the given script and loads file(s) under provided path.
 static func load_database(database_script: String, path: String) -> TextDatabase:
-	var storage := load(database_script).new() as TextDatabase
+	@warning_ignore("unsafe_method_access")
+	var storage: TextDatabase = load(database_script).new()
 	assert(storage, "Invalid custom script: %s" % database_script)
 	storage.load_from_path(path)
 	return storage
 
 ## Loads data from the given path. Can be called multiple times on different files and the new data will be appended to the database with incrementing IDs.
 ## If the path is a directory, all files from that directory will be loaded, in alphabetical order.
-func load_from_path(path: String):
-	var dir := DirAccess.open(path)
+func load_from_path(path: String) -> void:
+	var dir: DirAccess = DirAccess.open(path)
 	if dir:
-		var file_list: Array[String]
+		var file_list: Array[String] = []
 		
 		dir.list_dir_begin()
-		var file := dir.get_next()
+		var file: String = dir.get_next()
 		while not file.is_empty():
 			if not dir.current_is_dir():
 				file_list.append(dir.get_current_dir().path_join(file))
 			file = dir.get_next()
 		
 		file_list.sort()
-		for file_path in file_list:
+		for file_path: String in file_list:
 			load_from_path(file_path)
 		
 		return
@@ -171,20 +181,21 @@ func load_from_path(path: String):
 	
 	match path.get_extension():
 		"json":
-			var file := FileAccess.open(path, FileAccess.READ)
+			var file: FileAccess = FileAccess.open(path, FileAccess.READ)
+			@warning_ignore("untyped_declaration")
 			var json = JSON.parse_string(file.get_as_text())
 			
 			assert(json, "Parse failed, invalid JSON file: %s" % path)
 			assert(json is Array, "Invalid data type. Only JSON arrays are supported.")
 			data = json
 			
-			for i in data.size():
+			for i: int in data.size():
 				assert(data[i] is Dictionary, "Invalid data type. JSON array must contain only Dictionaries.")
 				data[i][id_name] = __last_id
 				__last_id += 1
 		"cfg":
-			var file := ConfigFile.new()
-			var error := file.load(path)
+			var file: ConfigFile = ConfigFile.new()
+			var error: Error = file.load(path)
 			assert(error == OK, "Parse failed, invalid ConfigFile. Error code: %s" % error)
 			
 			data = __config_file_to_array(file)
@@ -192,11 +203,13 @@ func load_from_path(path: String):
 			push_error("Unrecognized extension '.%s', can't extract data." % path.get_extension())
 			return
 	
-	for entry in data:
-		for property in __default_values:
+	for entry: Dictionary in data:
+		for property: Array in __default_values:
 			if not property in entry:
+				@warning_ignore("untyped_declaration")
 				var default = __default_values[property]
 				if default is Array or default is Dictionary:
+					@warning_ignore("unsafe_method_access")
 					entry[property] = default.duplicate()
 				else:
 					entry[property] = default
@@ -209,17 +222,18 @@ func load_from_path(path: String):
 		if not entry_name.is_empty() and not entry_name in entry:
 			push_warning("Entry has no name key (%s): %s" % [entry_name, entry])
 		
-		for property in __mandatory_properties:
+		for property: Array in __mandatory_properties:
 			var property_name: String = property[0]
 			
 			if property_name in entry:
 				if is_typed:
+					@warning_ignore("unsafe_call_argument")
 					assert(__match_type(typeof(entry[property_name]), property[1]), "Invalid type of property '%s' in entry '%s'." % [property_name, entry.get(entry_name)])
 			else:
 				assert(false, "Missing mandatory property '%s' in entry '%s'." % [property_name, entry.get(entry_name)])
 		
 		if is_validated:
-			for property in entry:
+			for property: String in entry:
 				assert(is_property_valid(entry, property), "Invalid property '%s' in entry '%s'." % [property[0], entry[entry_name]])
 		
 		if postprocess_entry.is_valid():
@@ -240,20 +254,20 @@ var __mandatory_properties: Array[Array]
 var __valid_properties: Array[Array]
 var __default_values: Dictionary
 
-func _init():
+func _init() -> void:
 	_initialize()
 
-func __setup():
+func __setup() -> void:
 	if not OS.is_debug_build():
 		return
 	
 	assert(not id_name.is_empty(), "'id_name' can't be empty String.")
 	
-	for property in __mandatory_properties:
+	for property: Array in __mandatory_properties:
 		if property[1] != TYPE_MAX:
 			is_typed = true
 	
-	for property in __valid_properties:
+	for property: Array in __valid_properties:
 		if property[1] != TYPE_MAX:
 			is_typed = true
 	
@@ -270,25 +284,25 @@ func __match_type(type1: int, type2: int) -> bool:
 		return type1 == type2 or (type1 == TYPE_INT and type2 == TYPE_FLOAT)
 
 func __property_exists(property: String) -> bool:
-	for p in __mandatory_properties:
+	for p: Array in __mandatory_properties:
 		if p[0] == property:
 			return true
 
-	for p in __valid_properties:
+	for p: Array in __valid_properties:
 		if p[0] == property:
 			return true
 	
 	return false
 
 func __config_file_to_array(data: ConfigFile) -> Array[Dictionary]:
-	var array: Array[Dictionary]
-	for section in data.get_sections():
-		var entry := {id_name: __last_id}
+	var array: Array[Dictionary] = []
+	for section: String in data.get_sections():
+		var entry: Dictionary = {id_name: __last_id}
 		if not entry_name.is_empty():
 			entry[entry_name] = section
 		__last_id += 1
 		
-		for value in data.get_section_keys(section):
+		for value: String in data.get_section_keys(section):
 			entry[value] = data.get_value(section, value)
 		
 		array.append(entry)
